d90a3deabdf42be55eadaeabc4a3781525e3fa82
Bruno dos santos <bruno16082006@gmail.com>
2026-06-02 22:35:20 -0300

    criando modo escuro

--- script/index.js
+++ script/index.js
@@ -9,3 +9,23 @@ document.addEventListener('scroll', (e) => {
         menu.style.position = "static"
     }
 })
+
+function mudarTema(evento){
+    const main = document.querySelector('.principal')
+
+    aplicaTodos(document.querySelectorAll('section'), 'light-dark')
+    aplicaTodos(document.querySelectorAll('h2'), 'h2') 
+    aplicaTodos(document.querySelectorAll('.barra-externa'), 'light-dark')
+    aplicaTodos(document.querySelectorAll('.card'), 'darki')
+    aplicaTodos(document.querySelectorAll('.barra-interna'), 'bckl')
+
+
+    main.classList.toggle('darki')
+    menu.classList.toggle('dark-menu')
+}
+
+function aplicaTodos(elemento, classe){
+    elemento.forEach((e) => {
+        e.classList.toggle(`${classe}`)
+    })
+}
